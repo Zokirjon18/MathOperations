99ce3cc679648d5c537d28d06852103b07a1d148
Zokirjon18 <tolqinovzokirjon347@gmail.com>
2025-07-01 19:56:01 +0500

    subtraction method

--- ConsoleApp3/MathOperations.cs
+++ ConsoleApp3/MathOperations.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleApp3
+﻿namespace ConsoleApp3
 {
     internal class MathOperations
     {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num1, int num2) 
+        {
+            return num1 - num2; 
+        }
+
     }
 }
